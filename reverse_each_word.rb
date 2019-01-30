f96205b2d1717ae59d5158b056e6fc9022bd9c79
Daniel Shimield <dxshim90@gmail.com>
2019-01-30 10:56:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(string)
-  newarr = string.split(" ").collect do |word|
-    word.reverse!
+    newarr = []
+    string.split(" ").collect do |word|
+    newarr << word.reverse!
+    newarr.join(" ")
 end
 
 
